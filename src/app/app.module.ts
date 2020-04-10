71db7f0206ee81be387cd5dbe928949329d0b0b9
ishankrsingh <ishankrsingh@gmail.com>
2020-04-10 22:18:41 +0530

    first routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//router module used for setting up the application level routing
+import {RouterModule,Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +20,24 @@ import { BlogEditComponent } from './blog-edit/blog-edit.component';
     HomeComponent,
     BlogViewComponent,
     BlogCreateComponent,
-    BlogEditComponent
+    BlogEditComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //routerModule forRoot method to declare the possible routes in application
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'about',component:AboutComponent},
+      {path:'blog/:blogId',component:BlogViewComponent},
+      {path: 'create',component:BlogCreateComponent},
+      {path:'edit/:blogId',component:BlogEditComponent},
+      {path: '**',component:NotFoundComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
