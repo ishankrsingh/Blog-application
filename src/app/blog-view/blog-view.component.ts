dbfa38ac1ad9b2fc41365247c7e102b74c498ff6
ishankrsingh <ishankrsingh@gmail.com>
2020-04-11 08:22:33 +0530

    Manual data entry,routing and templating done

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//import route relate code
+import{ActivatedRoute , Router} from '@angular/router';
+
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
@@ -7,9 +10,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogViewComponent implements OnInit {
 
-  constructor() { }
+  //empty variable
+  public currentBlog;
 
-  ngOnInit(): void {
+  //declare a dummy blog variable here
+  public allBlogs=[
+    {
+      "blogId":"1",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["comedy","humour"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"this is blog body ",
+      "description": "this is blog 1 description",
+      "title":"This is blog 1"
+    },
+    {
+      "blogId":"2",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["comedy","humour"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"<h1>this is big text</h1><p>this is small text</p> ",
+      "description": "this is the description of an example blog",
+      "title":"This is an example log"
+    },
+    {
+      "blogId":"3",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["dark","horror"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"this is blog body ",
+      "description": "this is blog 3 description",
+      "title":"This is blog 3"
+    }
+    
+  ]
+  
+  constructor(private _route: ActivatedRoute,private router: Router) {
+    console.log("constructor is called");
+   }
+
+  ngOnInit():void {
+    console.log("ngOnInitCalled");
+    //getting blog id from the route
+    let myBlogId=this._route.snapshot.paramMap.get('blogId');
+    console.log(myBlogId);
+    //calling the function to get the blog with this BlogId out of the overall array
+    this.getSingleBlogInformation(myBlogId);
   }
+
+  public getSingleBlogInformation(currentBlogId):any {
+    //using a for of loop here from typescript
+
+    for(let blog of this.allBlogs)
+    {
+      if(blog.blogId==currentBlogId)
+      {this.currentBlog=blog;
+      }
+    }
+    console.log(this.currentBlog);
+  }//end get blog information function
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-//this is a be default statement . It is like headerfile in C++
+//this is a be default statement . It is like headerfile in C++ ..
 
 import { Component, OnInit } from '@angular/core';
 
@@ -11,6 +11,49 @@ import { Component, OnInit } from '@angular/core';
 //simple class
 export class HomeComponent implements OnInit {
 
+  //declare a dummy blog variable here
+  public allBlogs=[
+    {
+      "blogId":"1",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["comedy","humour"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"this is blog body ",
+      "description": "this is blog 1 description",
+      "title":"This is blog 1"
+    },
+    {
+      "blogId":"2",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["comedy","humour"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"<h1>this is big text</h1><p>this is small text</p> ",
+      "description": "this is the description of an example blog",
+      "title":"This is an example log"
+    },
+    {
+      "blogId":"3",
+      "lastModified":"2017-10-20T12:20:47.854Z",
+      "created":"2017-10-20T12:20:47.854Z",
+      "tags":["dark","horror"],
+      "author":"Admin",
+      "category":"Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml":"this is blog body ",
+      "description": "this is blog 3 description",
+      "title":"This is blog 3"
+    }
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
